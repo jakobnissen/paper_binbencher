930d9b530c90ebae60f50ef640c17ff0ed58b013
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2024-03-12 13:38:06 +0100

    Run CheckM2
    Using the bin files created in the previous step, run CheckM2 v1.0.2.

---
+++ src/checkm2_split.sh
@@ -0,0 +1,2 @@
+module load checkm2/1.0.2;
+checkm2 predict --threads 20 --input results/vambout/bins_split/ --output-directory results/checkm2_split --database_path /home/projects/ku_00197/data/Databases/CheckM2_database/uniref100.KO.1.dmnd
