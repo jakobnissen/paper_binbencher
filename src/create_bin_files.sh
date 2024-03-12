1cc2a8dc20930f5d8e4ba3c669d8ae14b1eb92d0
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2024-03-12 13:36:28 +0100

    Create directory with VAMB bins in FASTA format
    This is the format used by CheckM2. We use the script in VAMB, commit
    8a9d0616 (i.e. not VAMB v4.1.3).

---
+++ src/create_bin_files.sh
@@ -0,0 +1,2 @@
+python ~/vamb/src/create_fasta.py ../../../data/vamb/cami2_gi/contigs_2kbp.fna results/vambout/split_header.tsv 200000 tmp/bins_split &
+python ~/vamb/src/create_fasta.py ../../../data/vamb/cami2_gi/contigs_2kbp.fna results/vambout/unsplit_header.tsv 200000 tmp/bins_unsplit &
