125c876b39d13c20f77f9cdd6814de085e9e21b1
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2024-03-12 13:31:37 +0100

    Run VAMB 4.1.3
    Note that the input files are not uploaded for reproducibility, but the
    output of vamb is in raw/vae_clusters.tsv.
    These must then be manually binsplit, if wanted.

---
+++ src/vamb.sh
@@ -0,0 +1,2 @@
+module load vamb/4.1.3;
+vamb --outdir results/vambout --fasta /home/projects/ku_00197/data/vamb/cami2_gi/contigs_2kbp.fna --bamfiles /home/projects/ku_00197/data/vamb/cami2_gi/bam_sorted/*.bam -p 10
